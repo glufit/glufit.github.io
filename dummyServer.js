0124ff56bace3112c1cb8596c457e0f7e9024369
Bernardo Arevalo <nardoarevalo@me.com>
2016-07-04 18:36:51 -0700

    Redirecting express app at index.html

--- dummyServer.js
+++ dummyServer.js
@@ -4,9 +4,14 @@ var app     = express();
 app.set('port', (process.env.PORT || 5000));
 
 //For avoidong Heroku $PORT error
-app.get('/ewkjfweijfwekfjlwek', function(request, response) {
-  var result = 'App is running'
-  response.send(result);
+
+
+var renderIndex = (req: express.Request, res: express.Response) => {
+  res.sendFile(path.resolve(__dirname, 'index.html'));
+}
+
+app.get('/', function(request, response) {
+  response.send("index.html");
 }).listen(app.get('port'), function() {
   console.log('App is running, server is listening on port ', app.get('port'));
 });
